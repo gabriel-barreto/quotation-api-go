b69b397f99a8599e84b2b7f00f60e47ca304bc17
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-10-12 20:58:56 -0300

    initial

---
+++ client/client.go
@@ -0,0 +1,6 @@
+package client
+
+func main() {
+	// TODO call the endpoint
+	// TODO store the response in a txt file
+}
---
+++ server/server.go
@@ -0,0 +1,9 @@
+package server
+
+func main() {
+	// TODO request the quotation of USD-BRL
+	// TODO store the quotation in the database
+	// TODO wrap the request in a timeout context (200ms)
+	// TODO wrap the storage in a timeout context (10ms)
+	// TODO Add a cache layer to read directly from the DB if is the same day
+}
