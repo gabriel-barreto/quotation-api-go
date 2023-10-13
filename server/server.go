ae3e1da0484f420e894e4c6f35addd6147e78232
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-10-12 22:17:32 -0300

    feat: storing the current quotation in the DB

--- server/server.go
+++ server/server.go
@@ -6,16 +6,75 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Quotation struct {
-	Value     string `json:"ask"`
-	Timestamp string `json:"timestamp"`
+	ID        string `gorm:"primaryKey"`
+	Value     float64
+	Timestamp int64 `gorm:"index"`
 }
 
 type QuotationResponse struct {
-	USDBRL Quotation
+	USDBRL struct {
+		Value     string `json:"ask"`
+		Timestamp string `json"timestamp"`
+	}
+}
+
+func createQuotation(r QuotationResponse) (*Quotation, error) {
+	value, err := strconv.ParseFloat(r.USDBRL.Value, 0)
+	if err != nil {
+		return &Quotation{}, nil
+	}
+	timestamp, err := strconv.ParseInt(r.USDBRL.Timestamp, 10, 0)
+	if err != nil {
+		return &Quotation{}, nil
+	}
+	return &Quotation{
+		ID:        uuid.New().String(),
+		Value:     value,
+		Timestamp: timestamp,
+	}, nil
+}
+
+func setupDB() (*gorm.DB, error) {
+	dbName := os.Getenv("SQLITE_DATABASE_NAME")
+	db, err := gorm.Open(sqlite.Open(dbName), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(&Quotation{})
+	return db, err
+}
+
+func saveQuotation(db *gorm.DB, quotation *Quotation) (*Quotation, error) {
+	// instantiating context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	// check for the same timestamp
+	existingQuotation := Quotation{}
+	res := db.
+		WithContext(ctx).
+		Model(existingQuotation).
+		Where("timestamp = ?", quotation.Timestamp).
+		Find(&existingQuotation)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	if res.RowsAffected == 1 {
+		return &existingQuotation, nil
+	}
+
+	// creating new record
+	err := db.WithContext(ctx).Model(&quotation).Create(quotation).Error
+	return quotation, err
 }
 
 func getCurrentQuotation() (*Quotation, error) {
@@ -31,22 +90,29 @@ func getCurrentQuotation() (*Quotation, error) {
 		return &Quotation{}, err
 	}
 	defer res.Body.Close()
-	var currentQuotation QuotationResponse
-	err = json.NewDecoder(res.Body).Decode(&currentQuotation)
+	var quotationResponse QuotationResponse
+	err = json.NewDecoder(res.Body).Decode(&quotationResponse)
 	if err != nil {
 		return &Quotation{}, err
 	}
-	return &currentQuotation.USDBRL, nil
+	return createQuotation(quotationResponse)
 }
 
 func Perform() {
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalf("Error while trying to connect with the database (%s)", err)
+	}
 	currentQuotation, err := getCurrentQuotation()
 	if err != nil {
 		log.Fatalf("Error while trying to get the current quotation: %s", err)
 	}
+	currentQuotation, err = saveQuotation(db, currentQuotation)
+	if err != nil {
+		log.Fatalf("Error while trying to store the current quotation: %s", err)
+	}
 	log.Println(currentQuotation)
 
-	// TODO store the quotation in the database
 	// TODO wrap the request in a timeout context (200ms)
 	// TODO wrap the storage in a timeout context (10ms)
 	// TODO Add a cache layer to read directly from the DB if is the same day
