903ca556ff19cdb561efb7d533f4ac90c207bec7
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-11-12 22:16:43 -0300

    feat: add initializer

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"os"
+	"strings"
 
+	"github.com/gabriel-barreto/go-quoting-api/client"
 	"github.com/gabriel-barreto/go-quoting-api/server"
 	"github.com/joho/godotenv"
 )
 
+func findArgTarget(args []string) (string, error) {
+	for _, arg := range args {
+		if strings.Contains(arg, "--") {
+			return arg, nil
+		}
+	}
+	return "", errors.New("unknown target")
+}
+
 func main() {
-	err := godotenv.Load(".env")
+	target, err := findArgTarget(os.Args)
+	if err != nil {
+		log.Fatalf("Error while trying to get command target: %s", err)
+	}
+	err = godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error while trying to load .env file: %s", err)
 	}
-	server.Perform()
+	if target == "--server" {
+		server.Start()
+	}
+	if target == "--client" {
+		client.Perform()
+	}
 }
