2c5620ce7f51174517dc8720f2a007491195f0f1
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-11-12 22:16:32 -0300

    feat: add client

--- client/client.go
+++ client/client.go
@@ -1,6 +1,43 @@
 package client
 
-func main() {
-	// TODO call the endpoint
-	// TODO store the response in a txt file
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gabriel-barreto/go-quoting-api/shared/models"
+)
+
+func getQuotation() (*models.Quotation, error) {
+	res, err := http.Get("http://localhost:3000/quote")
+	currQuotation := models.Quotation{}
+	if err != nil {
+		return &currQuotation, err
+	}
+	defer res.Body.Close()
+	json.NewDecoder(res.Body).Decode(&currQuotation)
+	return &currQuotation, nil
+}
+
+func persistToTxtFile(currQuotation *models.Quotation) error {
+	fileContent := fmt.Sprintf("Dólar hoje: USD %.2f", currQuotation.Value)
+	err := os.WriteFile("./current-quotation.txt", []byte(fileContent), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Perform() {
+	currQuotation, err := getQuotation()
+	if err != nil {
+		log.Fatalf("unable to get the current quotation: %s", err)
+	}
+	persistToTxtFile(currQuotation)
+	if err != nil {
+		log.Fatalf("unable to persist the current quotation: %s", err)
+	}
+	log.Println("Current quotation persisted!")
 }
